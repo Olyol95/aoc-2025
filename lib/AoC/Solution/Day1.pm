a42b88e9594f4a6b8f5f650694e7a358c9588baf
Oliver Youle <youle.oliver@gmail.com>
2025-12-01 19:43:02 +0000

    Tidy solutions for day 1

--- lib/AoC/Solution/Day1.pm
+++ lib/AoC/Solution/Day1.pm
@@ -10,39 +10,57 @@ with 'AoC::Solution';
 sub part_1 {
     my $self = shift;
 
-    my ($total, $zeroes) = (50, 0);
+    my ($total, $answer) = (50, 0);
     foreach my $instr (@{ $self->input }) {
-        next unless $instr =~ /(L|R)(\d+)/;
-        if ($1 eq 'L') {
-            $total = ($total - $2) % 100;
-        }
-        else {
-            $total = ($total + $2) % 100;
-        }
-        $zeroes++ if $total == 0;
+        ($total, my $zeroes) = turn_dial($total, $instr);
+        $answer++ if $total == 0;
     }
 
-    return $zeroes;
+    return $answer;
 }
 
 sub part_2 {
     my $self = shift;
 
-    my ($total, $zeroes) = (50, 0);
+    my ($total, $answer) = (50, 0);
     foreach my $instr (@{ $self->input }) {
-        next unless $instr =~ /(L|R)(\d+)/;
-        foreach my $tick (1..$2) {
-            if ($1 eq 'L') {
-                $total = ($total - 1) % 100;
-            }
-            else {
-                $total = ($total + 1) % 100;
-            }
-            $zeroes++ if $total == 0;
-        }
+        ($total, my $zeroes) = turn_dial($total, $instr);
+        $answer += $zeroes;
     }
 
-    return $zeroes;
+    return $answer;
+}
+
+sub turn_dial {
+    my ($start, $instr) = @_;
+
+    my ($total, $zeroes) = ($start, 0);
+    foreach my $tick (1..$instr->{amount}) {
+        if ($instr->{dir} eq 'L') {
+            $total = ($total - 1) % 100;
+        }
+        else {
+            $total = ($total + 1) % 100;
+        }
+        $zeroes++ if $total == 0;
+    }
+
+    return ($total, $zeroes);
+}
+
+sub _parse_input {
+    my $self = shift;
+
+    my @input;
+    foreach my $line (split("\n", $self->input)) {
+        next unless $line =~ /(L|R)(\d+)/;
+        push @input, {
+            dir    => $1,
+            amount => $2,
+        };
+    }
+
+    return \@input;
 }
 
 1;
