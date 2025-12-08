dd73b9e2c23f6c3ecc64f4cab9fd75d232a2d635
Oliver Youle <youle.oliver@gmail.com>
2025-12-08 12:43:21 +0000

    Add solutions for day 8

---
+++ lib/AoC/Solution/Day8.pm
@@ -0,0 +1,138 @@
+package AoC::Solution::Day8;
+
+use v5.42;
+use strictures 2;
+
+use List::Util qw(product);
+use Moo;
+
+with 'AoC::Solution';
+
+has iterations => (
+    is      => 'ro',
+    default => 1000,
+);
+
+sub part_1 {
+    my $self = shift;
+
+    my $distances = $self->_distances;
+    my @closest   = sort { $a <=> $b } keys %$distances;
+
+    my %circuits = %{ $self->input->{circuits} };
+    for (1..$self->iterations) {
+        my $distance = shift @closest;
+
+        my ($a_id, $b_id) = @{ $distances->{$distance} };
+        my $new_cid = $circuits{$a_id};
+        my $old_cid = $circuits{$b_id};
+
+        next if $new_cid == $old_cid;
+        foreach my $j_id (keys %circuits) {
+            my $c_id = $circuits{$j_id};
+            $circuits{$j_id} = $new_cid if $c_id == $old_cid;
+        }
+    }
+
+    my %totals;
+    foreach my $j_id (keys %circuits) {
+        my $c_id = $circuits{$j_id};
+        $totals{$c_id}++;
+    }
+
+    my @values = sort { $b <=> $a } values %totals;
+    return product @values[0..2];
+}
+
+sub part_2 {
+    my $self = shift;
+
+    my $distances = $self->_distances;
+    my @closest   = sort { $a <=> $b } keys %$distances;
+
+    my $answer;
+    my %circuits = %{ $self->input->{circuits} };
+    while (1) {
+        my $distance = shift @closest;
+
+        my ($a_id, $b_id) = @{ $distances->{$distance} };
+        my $new_cid = $circuits{$a_id};
+        my $old_cid = $circuits{$b_id};
+
+        next if $new_cid == $old_cid;
+
+        my %totals;
+        foreach my $j_id (keys %circuits) {
+            my $c_id = $circuits{$j_id};
+            if ($c_id == $old_cid) {
+                $circuits{$j_id} = $new_cid;
+                $totals{$new_cid}++;
+            }
+            else {
+                $totals{$c_id}++;
+            }
+        }
+
+        if (scalar keys %totals == 1) {
+            $answer = $self->input->{junctions}->{$a_id}->{x}
+                * $self->input->{junctions}->{$b_id}->{x};
+            last;
+        }
+    }
+
+    return $answer;
+}
+
+sub _distances {
+    my $self = shift;
+
+    my %distances;
+    my $total_junctions = scalar keys %{ $self->input->{junctions} };
+    foreach my $a_id (0..$total_junctions - 2) {
+        foreach my $b_id ($a_id + 1..$total_junctions - 1) {
+            my $distance = $self->_distance($a_id, $b_id);
+            $distances{$distance} = [$a_id, $b_id];
+        }
+    }
+
+    return \%distances;
+}
+
+sub _distance {
+    my ($self, $a_id, $b_id) = @_;
+
+    my ($a, $b) = (
+        $self->input->{junctions}->{$a_id},
+        $self->input->{junctions}->{$b_id},
+    );
+    return sqrt(
+        ($b->{x} - $a->{x}) ** 2
+            + ($b->{y} - $a->{y}) ** 2
+            + ($b->{z} - $a->{z}) ** 2
+    );
+}
+
+sub _parse_input {
+    my $self = shift;
+
+    my %input;
+
+    my $cid = 0;
+    foreach my $line (split("\n", $self->input)) {
+        chomp $line;
+        $line =~ s/^\s+//g;
+        next unless $line;
+        my ($x, $y, $z) = split(/,/, $line);
+        $input{junctions}{$cid} = {
+            x => $x,
+            y => $y,
+            z => $z,
+        };
+        $input{circuits}{$cid} = $cid;
+        $cid++;
+    }
+
+    return \%input;
+}
+
+1;
---
+++ t/AoC-Solution-Day8.t
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use v5.42;
+use strictures 2;
+
+use Test::More;
+
+use_ok('AoC::Solution::Day8');
+
+my $solution = AoC::Solution::Day8->new(
+    input => join("\n", q(
+        162,817,812
+        57,618,57
+        906,360,560
+        592,479,940
+        352,342,300
+        466,668,158
+        542,29,236
+        431,825,988
+        739,650,466
+        52,470,668
+        216,146,977
+        819,987,18
+        117,168,530
+        805,96,715
+        346,949,466
+        970,615,88
+        941,993,340
+        862,61,35
+        984,92,344
+        425,690,689
+    )),
+    iterations => 10,
+);
+
+is($solution->part_1, 40, 'part_1');
+is($solution->part_2, 25272, 'part_2');
+
+done_testing();
